bff4d4eb4408ac0d9137555ca8cd9757f4e51fec
ivhak <hakonseniver@yahoo.no>
2021-06-23 13:44:07 +0200

    Add a serial CPU version of the code

--- Makefile
+++ Makefile
@@ -4,6 +4,9 @@ CUDA_OUT := image_convolution_cuda
 HIP_SRC  := src/main.hip.cpp
 HIP_OUT  := image_convolution_hip
 
+SERIAL_SRC  := src/main.serial.c
+SERIAL_OUT  := image_convolution_serial
+
 LIBS := libs/bitmap.c libs/shared.c
 OBJ := $(patsubst %.c,%.o,$(LIBS))
 
@@ -21,6 +24,7 @@ endif
 
 hip: $(HIP_OUT)
 cuda: $(CUDA_OUT)
+serial: $(SERIAL_OUT)
 
 %.o: %.c
 	$(CC) -c $(FLAGS) $< -o $@
@@ -31,8 +35,12 @@ $(CUDA_OUT): $(OBJ) $(CUDA_SRC)
 $(HIP_OUT): $(OBJ) $(HIP_SRC)
 	$(HIP_CC) $(FLAGS) $^ -o $@
 
+$(SERIAL_OUT): $(OBJ) $(SERIAL_SRC)
+	$(CC) $(FLAGS) $^ -o $@
+
 clean:
 	rm -Rf $(OBJ)
 	rm -Rf $(CUDA_OUT)
 	rm -Rf $(HIP_OUT)
+	rm -Rf $(SERIAL_OUT)
 
---
+++ src/main.serial.c
@@ -0,0 +1,123 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "../libs/bitmap.h"
+#include "../libs/shared.h"
+
+
+// Apply convolutional kernel on image data
+void applyKernel(pixel **out, pixel **in, unsigned int width, unsigned int height, const int *kernel, unsigned int kernelDim, float kernelFactor) {
+    unsigned int const kernelCenter = (kernelDim / 2);
+    for (unsigned int y = 0; y < height; y++) {
+        for (unsigned int x = 0; x < width; x++) {
+            unsigned int ar = 0, ag = 0, ab = 0;
+            for (unsigned int ky = 0; ky < kernelDim; ky++) {
+                int nky = kernelDim - 1 - ky;
+                for (unsigned int kx = 0; kx < kernelDim; kx++) {
+                    int nkx = kernelDim - 1 - kx;
+
+                    int yy = y + (ky - kernelCenter);
+                    int xx = x + (kx - kernelCenter);
+                    if (xx >= 0 && xx < (int) width && yy >=0 && yy < (int) height) {
+                        ar += in[yy][xx].r * kernel[nky * kernelDim + nkx];
+                        ag += in[yy][xx].g * kernel[nky * kernelDim + nkx];
+                        ab += in[yy][xx].b * kernel[nky * kernelDim + nkx];
+                    }
+                }
+            }
+            if (ar || ag || ab) {
+                ar *= kernelFactor;
+                ag *= kernelFactor;
+                ab *= kernelFactor;
+                out[y][x].r = (ar > 255) ? 255 : ar;
+                out[y][x].g = (ag > 255) ? 255 : ag;
+                out[y][x].b = (ab > 255) ? 255 : ab;
+            } else {
+                out[y][x].r = 0;
+                out[y][x].g = 0;
+                out[y][x].b = 0;
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+    /*
+       Parameter parsing, don't change this!
+       */
+    unsigned int iterations = 1;
+    char *output = NULL;
+    char *input = NULL;
+    unsigned int filterIndex = 2;
+    int ret = 0;
+
+    parse_args(argc, argv, &iterations, &filterIndex, &output, &input);
+    /*
+       End of Parameter parsing!
+       */
+
+
+    /*
+       Create the BMP image and load it from disk.
+       */
+    bmpImage *image = newBmpImage(0,0);
+    if (image == NULL) {
+        fprintf(stderr, "Could not allocate new image!\n");
+        goto error_exit;
+    }
+
+    if (loadBmpImage(image, input) != 0) {
+        fprintf(stderr, "Could not load bmp image '%s'!\n", input);
+        freeBmpImage(image);
+        goto error_exit;
+    }
+
+    printf("Apply kernel '%s' on image with %u x %u pixels for %u iterations\n", filterNames[filterIndex], image->width, image->height, iterations);
+
+
+    // TODO: implement time measurement from here
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_MONOTONIC, &start_time);
+
+    // Here we do the actual computation!
+    // image->data is a 2-dimensional array of pixel which is accessed row first ([y][x])
+    // each pixel is a struct of 3 unsigned char for the red, blue and green colour channel
+    bmpImage *processImage = newBmpImage(image->width, image->height);
+    for (unsigned int i = 0; i < iterations; i ++) {
+        applyKernel(processImage->data,
+                image->data,
+                image->width,
+                image->height,
+                filters[filterIndex],
+                filterDims[filterIndex],
+                filterFactors[filterIndex]
+                );
+        swapImage(&processImage, &image);
+    }
+    freeBmpImage(processImage);
+
+    clock_gettime(CLOCK_MONOTONIC, &end_time);
+    float spentTime = ((end_time.tv_sec - start_time.tv_sec)) + ((end_time.tv_nsec - start_time.tv_nsec)) * 1e-9;
+    printf("Time spent: %.3f seconds\n", spentTime);
+
+
+    //Write the image back to disk
+    if (saveBmpImage(image, output) != 0) {
+        fprintf(stderr, "Could not save output to '%s'!\n", output);
+        freeBmpImage(image);
+        goto error_exit;
+    };
+
+graceful_exit:
+    ret = 0;
+error_exit:
+    if (input)
+        free(input);
+    if (output)
+        free(output);
+    return ret;
+};
